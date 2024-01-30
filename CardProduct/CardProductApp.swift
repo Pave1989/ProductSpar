532a001971e6cb75ffa41aaf357126c40d474f91
Pavel <nuar89@mail.ru>
2024-01-30 22:00:27 +0300

    Initial Commit

---
+++ CardProduct/CardProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardProductApp.swift
+//  CardProduct
+//
+//  Created by Павел Галкин on 30.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct CardProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
